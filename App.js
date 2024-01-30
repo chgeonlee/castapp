76cf5047cde3a17ccefeef22c34c6b281bf08352
chunggeonlee <chgeon.lee@aqara.kr>
2024-01-30 16:58:07 +0900

    init

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useFonts } from "expo-font";
+import Stage from "./src/stage";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Railway-Medium": require("./assets/fonts/Raleway-Medium.ttf"),
+    "Railway-Bold": require("./assets/fonts/Raleway-Bold.ttf"),
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stage />
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
